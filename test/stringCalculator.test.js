7105d1715f2d143c59d2a8d36faa084af07dc734
PurviVaghela <purvi09v@gmail.com>
2024-08-19 16:19:53 +0530

    single number

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,8 +3,14 @@ const { add } = require('../stringCalculator');
 
 describe('String Calculator', () => {
 
+    //empty string
     it('should return 0 for an empty string', () => {
         expect(add("")).to.equal(0);
     });
+    // single number
+    it('should return the number itself for a single number', () => {
+        expect(add("1")).to.equal(1);
+    });
+
 
 });
